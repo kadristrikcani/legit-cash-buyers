4ba0c517151f68c01814a52456f5652b5dcf8f2f
Bledi Piperku <Bledip2003@gmail.com>
2024-07-10 13:49:55 +0200

    removed unused use client

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,12 +11,12 @@ interface Props {
 
 export default function MainLayout({ children }: Props) {
   return (
-    <div>
+    <>
       <div className="absolute left-[-10rem] top-[-1rem] -z-10 h-[31.25rem] w-[68rem] rounded-full bg-burntSienna/30 blur-[10rem] sm:w-[500px] md:w-[600px] lg:left-[-8rem]" />
       <div className="absolute right-[0rem] top-[-6rem] -z-10 h-[31.25rem] w-[45rem] rounded-full bg-[goldenrod]/30 blur-[10rem] sm:w-[250px]" />
       <Header typeformId={headerString.typeformId} />
       {children}
       <Footer typeformId={footerString.typeformId} />
-    </div>
+    </>
   )
 }
--- src/app/(main)/testimonials/index.tsx
+++ src/app/(main)/testimonials/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from 'react'
 
 import TestimonialCarousel, { ITestimonial } from '@src/components/testimonials'
